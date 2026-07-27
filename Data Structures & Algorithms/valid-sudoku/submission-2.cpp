1eda0f024b939ae53dd1dfd66c40d2d4f6eb9368
Ashwin-Acharya-388 <ashwinacharya388@gmail.com>
2026-07-27 19:11:22 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<9;i++){
+            unordered_set<char>mp;
+            unordered_set<char>mp1;
+            for(int j=0;j<9;j++){
+                if(board[j][i]=='.')continue;
+                if(mp.count(board[j][i])){
+                    return false;
+                }
+                mp.insert(board[j][i]);
+            }
+            for(int j=0;j<9;j++){
+                 if(board[i][j]=='.')continue;
+                if(mp1.count(board[i][j])){
+                    return false;
+                }
+                mp1.insert(board[i][j]);
+            }
+        }
+        for(int sq=0;sq<9;sq++){
+            unordered_set<char>seen;
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    int row = (sq/3)*3 +i;
+                    int col=(sq%3)*3 +j;
+                    if(board[row][col]=='.')continue;
+                    if(seen.count(board[row][col]))return false;
+                    seen.insert(board[row][col]);
+                }
+            }
+        }
+        return true;
+    }
+};
