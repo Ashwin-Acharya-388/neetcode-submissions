c1e5cedd6366eaff2a46333c85193419459717b5
Ashwin-Acharya-388 <ashwinacharya388@gmail.com>
2026-08-24 11:13:39 +0530

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    unordered_set<int>visit;
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<vector<int>>adj(n);
+        for(auto &ed : edges){
+            adj[ed[0]].push_back(ed[1]);
+            adj[ed[1]].push_back(ed[0]);
+        }
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(visit.count(i)){
+                continue;
+            }
+            dfs(adj,i);
+            res++;
+        }
+        return res;
+    }
+    void dfs(vector<vector<int>>&adj,int i){
+        if(visit.count(i)){
+            return;
+        }
+        visit.insert(i);
+        for(int x : adj[i]){
+            if(visit.count(x)){
+                continue;
+            }
+            dfs(adj,x);
+        }
+    }
+
+};
