7b8cca5c3ef8dead5f6483fe288918ca42e074a5
Ashwin-Acharya-388 <ashwinacharya388@gmail.com>
2026-09-16 18:53:24 +0530

    Add: course-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-4.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int m = prerequisites.size(),n= prerequisites[0].size();
+        vector<vector<int>>adj(numCourses);
+        vector<int>indegree(numCourses);
+        for(auto& pre : prerequisites){
+            adj[pre[1]].push_back(pre[0]);
+            indegree[pre[0]]++;
+        }
+        queue<int>q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>res;
+        while(!q.empty()){
+            int x = q.front();
+            q.pop();
+            res.push_back(x);
+            for(int ad : adj[x]){
+                indegree[ad]--;
+                if(indegree[ad]==0){
+                    q.push(ad);
+                }
+            }
+        }
+        
+        if(res.size()==numCourses){
+            return res;
+        }
+        return {};
+    }
+};
